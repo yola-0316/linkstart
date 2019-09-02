3b5c75bc12175f64a850b963c41158d410984bc9
Yoru <yr6kami@gmail.com>
2019-09-02 13:09:09 +0800

    chore: update dependency

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
+
+Sentry.init({ dsn: 'https://ea868775913f4319ac9c269633245093@sentry.io/1545127' });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 import Home from './pages/Home';
+import Hooks from './pages/Hooks';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+    exact: true,
+  },
+  {
+    path: '/hooks',
+    name: 'Hooks',
+    component: Hooks,
     exact: true,
   },
 ];
