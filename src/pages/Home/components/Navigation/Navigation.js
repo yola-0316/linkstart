35f3a6d70b3e671c504ae65ad193653110144bad
Yoru <yr6kami@gmail.com>
2019-11-07 21:53:53 +0800

    chore: prepare migrate to typescript

--- src/pages/Home/components/Navigation/Navigation.js
+++ src/pages/Home/components/Navigation/Navigation.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { PrimaryButton } from 'office-ui-fabric-react';
 
 export default function Navigation() {
   return (
     <div className="navigation">
-      <section className="head" />
+      <section className="head">
+        <PrimaryButton>新建</PrimaryButton>
+      </section>
       <section className="body" />
+      <div className="modal">
+        <form action="">
+          <div>
+            <label htmlFor="">类别</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">名称</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">链接</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">描述</label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
