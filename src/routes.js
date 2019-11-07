a925f5b47d600b8c9847ec95e68f30a2e873e2d8
Yoru <yr6kami@gmail.com>
2019-11-07 13:48:51 +0800

    chore: remove unused file

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,10 @@
 import Home from './pages/Home';
-import Hooks from './pages/Hooks';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
-    exact: true,
-  },
-  {
-    path: '/hooks',
-    name: 'Hooks',
-    component: Hooks,
     exact: true,
   },
 ];
